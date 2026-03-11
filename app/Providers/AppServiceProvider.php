b9826813ae4ce525bed3109e9de68619ff5fa7b6
longmanngithub <69972928+longmanngithub@users.noreply.github.com>
2026-03-11 10:29:41 +0700

    feat: Integrate Brevo mailer service by adding necessary Symfony packages and configuring mail and service providers.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Mail;
+use Symfony\Component\Mailer\Bridge\Brevo\Transport\BrevoTransportFactory;
+use Symfony\Component\Mailer\Transport\Dsn;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +27,15 @@ class AppServiceProvider extends ServiceProvider
         
         // Load migrations from shared database folder
         $this->loadMigrationsFrom(base_path('../database/migrations'));
+
+        Mail::extend('brevo', function () {
+            return (new BrevoTransportFactory())->create(
+                new Dsn(
+                    'brevo+api',
+                    'default',
+                    config('services.brevo.key')
+                )
+            );
+        });
     }
 }
